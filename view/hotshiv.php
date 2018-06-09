86760a5f6b3a6d12ad0c2faa8de6dde16378e7bf
D.Franck <djacotofranck@gmail.com>
2018-06-09 02:40:04 +0200

    formulaire ajouté

---
+++ view/hotshiv.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<!--css-->
+		<link rel="stylesheet" type="text/css" href="../hotshiproj/assets/css/hotshicss.css">
+		<link rel="stylesheet" type="text/css" href="../hotshiproj/assets/css/style.css">
+	</head>
+
+	<body class="bg-light">
+		<div class="container">
+			<div class="row">
+				<div class="offset-2 col-8">
+
+					<form style="margin-top: 15%;">
+						<fieldset class="form_fildset">
+							<legend class="fildset_legend"> 
+								<div class="logo">
+									<img src="../hotshiproj/assets/img/logo-landing-2.png" style="height: 100px;" >
+								</div>		
+							</legend>
+
+							<div class="button_set">
+								<div class="mx-2 row">
+									<div class=" col-6">
+										<button class="btn btn-primary btn-lg button_width" >Begin the work</button>
+									</div>
+
+									<div class="col-6">
+										<button class="btn btn-lg btn-danger button_width">Cancel the work</button>
+									</div>
+								</div>
+							</div>
+
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
