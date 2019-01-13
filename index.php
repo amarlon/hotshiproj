48fd350728c862b9d5fc0ff12b22ef72816c4638
Anaelmarlon Luzayamo <39515872+amarlon@users.noreply.github.com>
2019-01-12 19:48:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
-include('controleur/hosthicheck.php');
+include('controleur/system_check.php');
 $uri= $_SERVER['REQUEST_URI']; // /Code/LT1234/
 $uri =trim($uri, '/');
 $uri = explode('/', $uri);
 if (isset($uri[1])) {
     $uri[1]=$uri[1];
 }else{
-    $uri[1]='hotshi';
+    $uri[1]='home';
 }
 switch ($uri[1])
 {
     case "index.php":
-        $hotshi->index(); 
+        $system->index(); 
         break;
     case "time":
-        $hotshi->time();
+        $system->time();
         break;
     case "cake":
         echo "i est un gateau";
         break;
     default:
-         $hotshi->index(); 
+         $system->index(); 
 }
